1210819dc917108bbb63ac16c648b9f09d270b3e
Luiz Claudio <luizcns18@gmail.com>
2018-02-06 19:41:20 -0300

    feat: Add TabNavigation component

--- App.js
+++ App.js
@@ -1,17 +1,62 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Platform } from 'react-native';
 import AddDeck from './components/AddDeck';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
 import DecksList from './components/DecksList';
+import { TabNavigator } from 'react-navigation';
+import { purple, white } from './utils/colors';
+import { MaterialIcons } from '@expo/vector-icons';
+
+
+const Tabs = TabNavigator({
+  DecksList: {
+    screen: DecksList,
+    navigationOptions: {
+      tabBarLabel: 'Decks',
+      tabBarIcon: ({ tintColor }) => <MaterialIcons name='list' size={26} color={tintColor} />
+    }
+  },
+  AddDeck: {
+    screen: AddDeck,
+    navigationOptions: {
+      tabBarLabel: 'New Deck',
+      tabBarIcon: ({ tintColor }) => <MaterialIcons name='add-circle-outline' size={26} color={tintColor} />
+    }
+  }
+},{
+    navigationOptions: {
+      header: null
+    },
+    tabBarOptions: {
+      showIcon: true,
+      upperCaseLabel: false,
+      // showLabel: false,
+      activeTintColor: Platform.OS === 'ios' ? purple : white,
+      style: {
+        height: 57,
+        backgroundColor: Platform.OS === 'ios' ? white : purple,
+        shadowColor: 'rgba(0, 0, 0, 0.24)',
+        shadowOffset: {
+          width: 0,
+          height: 3
+        },
+        shadowRadius: 6,
+        shadowOpacity: 1,
+      },
+      tabStyle: {
+        flexDirection:'row'
+      }
+    }
+});
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{flex:1}}>
-          <DecksList />
+          <Tabs />
         </View>
       </Provider>
     );
--- utils/colors.js
+++ utils/colors.js
@@ -1,7 +1,7 @@
 export const purple = '#292477'
 export const gray = '#757575'
 export const white = '#fff'
-export const blacl = '#000'
+export const black = '#000'
 export const red = '#b71845'
 export const orange = '#f26f28'
 export const blue = '#4e4cb8'
