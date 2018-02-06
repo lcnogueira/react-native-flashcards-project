e41b08ae4b1794800c5ae2567a926e41404d07e8
Luiz Claudio <luizcns18@gmail.com>
2018-02-05 22:12:02 -0300

    feat: Provide store to the components

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import AddDeck from './components/AddDeck';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers';
 
 export default class App extends React.Component {
   render() {
     return (
-      <AddDeck />
+      <Provider store={createStore(reducer)}>
+          <AddDeck />
+      </Provider>
     );
   }
 }
